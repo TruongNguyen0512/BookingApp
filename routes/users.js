ec0bcb1220a62b18eeb4ea64bd0b5b74bcfce5e0
TruongNguyen0512 <nguyencbsw.thcs@gmail.com>
2024-03-14 12:55:03 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,17 @@
 const express =  require('express')
 const {updateUser,deleteUser,getUser,getAllUser} = require("../controller/user")
+const {verifyToken,verifyUser} = require("../util/verifyToken")
 const router = express.Router() 
 
+
+// Check authentication
+router.get("/checkauthentication",verifyToken,verifyUser,(req,res,next)=>{
+    res.send("hello user ,You are logged in")
+})
+
+router.get("/checkusers/:id",verifyUser,(req,res,next)=>{
+    res.send("hello user, you are logged in and you can delete your account")
+})
 
 // Update
 router.put("/:id", updateUser);
---
+++ util/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt =  require("jsonwebtoken") 
+const {createError} = require("../util/error")
+
+const verifyToken =  (req,res,next) =>{
+    const token = req.cookies.access_token
+    if(!token){
+        return next(createError(401,"You are not authenticatied"))
+    }
+    jwt.verify(token,"93478923DJSBKJSAHDIADA",(err,user)=>{
+        if(err) return next(createError(403,"Token is not valid"))
+        req.user = user 
+        next()
+    })
+}
+
+const verifyUser = (req,res,next) =>{
+    verifyToken(req,res,()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        }else{
+            return next(createError(403,"You are not authorized"))   
+        }
+    })
+}
+module.exports = {verifyToken,verifyUser}
